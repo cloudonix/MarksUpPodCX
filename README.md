a5ee3a471983dae1fadc6efbd1cb751a6d1fdde1
Oded Arbel <oded@geek.co.il>
2022-08-15 13:29:15 +0300

    First public release

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # MarksUpPodCX
-A static RSS generator for podcasts to be hosted on S3/CloudFront
+
+A static RSS generator for podcasts to be hosted on S3/CloudFront.
+
+This software can be used with an S3 bucket with website hosting enabled, optionally fronted by a CDN
+(such as CloudFront) to host a podcast. The RSS generator lambda function is setup to receive S3 update
+events, so that whenever a file is changed in the S3 bucket, the generator will immediately be called to
+parse the files and generate an RSS file for the podcast.
+
+Additionally podcast episodes may be prescheduled, so that they are only published at a future date.
+To facilitate future publishing, the RSS generator lambda should also be connected to an EventBridge that
+issues cron events at the required interval (currently future scheduling resolution is one day).
+
+## Installation
+
+1. Create an S3 bucket to host the podcast, and set it to:
+    - Use ACL
+    - Enable website hosting
+    
+2. publish the Lambda layer in `src/layer` by going into that directory and running `make`.
+
+2. Log into the AWS Lambda console and create a new Node.js x86_64 function:
+   
+    1. For the code content, put in the content of `src/lambda/index.js`
+    2. Add a new layer and choose "Custom" and the layer you created in step (2).
+    3. Add a new trigger and select:
+        * Type: S3
+        * Bucket: the bucket you created in step (1)
+        * Event type: all object create events
+    4. Add another trigger and select:
+        * Type: S3
+        * Bucket: the bucket you created in step (1)
+        * Event type: all object delete events
+    5. Add another trigger (this is optional and only needed if you want to schedule future publications):
+        * Type: EventBridge
+        * Rule: create a new rule
+        * Rule name and description: put something descriptive that works for you
+        * Rule type: schedule expression
+        * Schedule expression: `cron(0 1 * * ? *)`
+    6. Go to "Configuration" and then "Environment variables" and create the following variables:
+        * `podcast_name`: the name of your podcast (this field is not written into the RSS and is mainly used for logging)
+        * `bucket`: the name of the bucket you created in step (1)
+        * `baseURL`: a URL that can be used to access the root of the bucket - either the S3 hosted website URL or the URL you assigned in a CDN to access that origin.
+        * `author`: a name to be used for the RSS "author" entry
+        * `owner`: a name to be used for the RSS "owner" entry
+        * `owner_email`: a name to be used for the RSS "owner email" entry
+        * `categories`: a list of comma separated "categories" according to the iTunes category list, to be used for the iTunes-specific RSS categories list.
+    
+4. Optionally (but recommended) configure Lambda to send you error notifications by email:
+
+    1. Go to the AWS SNS console and create a new topic.
+    2. Subscribe your email address to the new topic
+    3. In the AWS Lambda console, in your new lambda function, click "Add Destination" and configure it:
+        * Source: Asynchronous invocation
+        * Condition: on failure
+        * Destination type: SNS topic
+        * Destination: select the topic you created
+
+## Usage
+
+### Create Podcast Descriptor
+
+The first thing to do is to create the "top level" podcast descriptor - this contains information that goes into the "`channel`" section of the RSS (i.e. the podcast details, in contrast to the per-episode details).
+
+To create the descriptor you need to create a few files and place them at the root of the S3 bucket:
+
+* Markdown file with a description and title:
+  * The first level headline (starting with a single `#`) will be used as the podcast's title. All other content will be rendered as the podcast description.
+* Image files to be used as the podcast images (thumbnails). These must be in PNG format and will be listed in name order. It is recommended to name all the files identically with a suffix specifying the dimensions of the file. For example `my-podcast-150.png`.
+
+### Create Episodes
+
+For each episode, create a folder names according to the desired publication date of that episode - using ISO 8601 date format (i.e. `YYYY-MM-DD` format, for example `2022-08-15`).
+
+In each episode folder, place the following files:
+
+- An MP3 file that is the podcast content.
+- A markdown file with a description, title and keywords:
+  - The first level headline (starting with a single `#`) will be used as the episode's title.
+  - If the last line starts with the text `Keywords:` the rest of the line will be understood as a comma-separated list of keywords to be used in the episode's RSS "keywords" tag.
+  - All other content will be rendered as the episode's description.
+- Image files to be used as the episodes images (thumbnails). These must be in PNG format and will be listed in name order. It is recommended to name all the files identically with a suffix specifying the dimensions of the file. For example `episode-1-150.png`.
+
+### Generate RSS
+
+Whenever the content of the bucket changes (and every 1AM UTC, if the EventBridge trigger was created), the generator will run to create a file named `rss` in the bucket top level directory - so that it can be loaded by accessing the `baseURL` configured for the generator with the suffix `/rss` - for example, if the base URL is set to `https://podcast.example.com` (and it is configured correctly), the RSS will be accessible using the URL `https://podcast.example.com/rss`.
+
+The RSS can then be published to podcast platforms.
+
---
+++ src/lambda/index.js
@@ -0,0 +1,329 @@
+const GeneratorVersion = '1.0';
+const GeneratorName = `MarksUpPodCX/${GeneratorVersion}`;
+const podcast_name = process.env.podcast_name;
+console.log('Starting',GeneratorName,'RSS generator for', podcast_name);
+
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const { v5: uuidv5 } = require('uuid');
+var mediaDuration = require('mp3-duration');
+
+const baseURL = process.env.baseURL || '';
+const author = process.env.author || '';
+const owner = process.env.owner || '';
+const owner_email = process.env.owner_email || '';
+const categories = (process.env.categories || '').split(/\s*,\s*/);
+
+Date.prototype.toRFC822 = function() { return this.toUTCString().replace('GMT', '+0000'); };
+
+async function listBucket(bucket, nextToken) {
+    let params = { Bucket: bucket };
+    if (nextToken)
+        params.ContinuationToken = nextToken;
+    let res = await s3.listObjectsV2(params).promise();
+    let files = res.Contents.map(obj => obj.Key);
+    if (!res.IsTruncated)
+        return files;
+    let remainder = await listBucket(bucket, res.NextContinuationToken);
+    return files.concat(remainder);
+}
+
+async function storeRSS(text, bucket, key) {
+	return await s3.putObject({
+		Bucket: bucket,
+		Key: key,
+		Body: text,
+		ContentType: "application/xml",
+		ACL: "public-read"
+	}).promise();
+}
+
+async function verifyPublicRead(bucket, key) {
+	let acl = await s3.getObjectAcl({ Bucket: bucket, Key: key}).promise();
+	let hasPublicRead = false;
+	acl.Grants.forEach(g => {
+		if (g.Permission == 'READ') {
+			hasPublicRead = true;
+		}
+	});
+	if (hasPublicRead)
+		return;
+	console.log('Need to allow public-read on', key);
+	await s3.putObjectAcl({ Bucket: bucket, Key: key, ACL: 'public-read' }).promise();
+}
+
+class PodItem {
+    bucket = '';
+    keyPrefix = '';
+    title = '';
+    description = '';
+    images = {};
+    media = '';
+	pubdate = new Date(0);
+	keywords = [];
+	
+	publishDate() {
+		return this.pubdate;
+	}
+
+	async loadDescriptor(path) {
+		let s3obj = await s3.getObject({ Bucket: this.bucket, Key: this.keyPrefix + path }).promise();
+		console.log('Loaded S3 object', path, s3obj);
+		this.pubdate = new Date(s3obj.LastModified);
+		return s3obj.Body.toString().replace(/\r/g, '').trim();
+	}
+	
+    async loadMarkdown(path) {
+        let text = await this.loadDescriptor(path);
+        let title, rest;
+        [title, ...rest] = text.split("\n");
+        if (title.startsWith('#')) {
+            this.title = title.replace(/#\s+/,'');
+        } else {
+            this.title = title;
+        }
+        while (rest[rest.length-1].trim() == '')
+			rest.pop();
+		if (rest[rest.length-1].toLowerCase().startsWith("keywords:"))
+			this.keywords = rest.pop().replace(/^keywords:/i,'').trim().split(/\s*,\s*/);
+        this.description = rest.join("\n").trim();
+    }
+    
+    async addFile(path) {
+        if (path.endsWith('.md'))
+            return await this.loadMarkdown(path);
+        else if (path.endsWith('.png'))
+            this.addImage(path);
+        else if (path.endsWith('.mp3'))
+            this.media = path;
+        else if (path == 'favicon.ico' || path == 'rss') {} // ignore expected files that are not to be processed
+        else {
+            console.log('Unrecognized extension when loading item file', path);
+			return;
+		}
+		// make sure that published media and images are accessible
+		await verifyPublicRead(this.bucket, this.keyPrefix + path);
+        return;
+    }
+    
+    addImage(path) {
+		let name,size,ext;
+		[name,size,ext] = path.split(/(?:-(?=\d+)|(?<=\d+)\.)/);
+		if (!size || !size.match(/^\d+$/)) {
+			console.log('Error parsing image file', path, '! Ignoring.');
+			return;
+		}
+		this.images[size] = path;
+	}
+}
+
+class Episode extends PodItem {
+    id = '';
+	mediaSize = 0;
+	duration = 0;
+	ready = false;
+	canPublish = new Date(0);
+	
+    constructor(bucket, id) {
+        super();
+        this.bucket = bucket;
+        this.id = id;
+        this.keyPrefix = `${id}/`;
+		this.canPublish = new Date(id);
+	}
+	
+    publishDate() {
+		return this.canPublish; // maybe the latest of this or this.pubdate? Eric said no.
+	}
+	
+	addKeywords(keywords) {
+		if (!keywords || !keywords.length)
+			return;
+		this.keywords = keywords.concat(this.keywords);
+	}
+    
+    async loadMediaMetadata(path) {
+		let s3obj = await s3.getObject({ Bucket: this.bucket, Key: this.keyPrefix + path }).promise();
+		console.log('Loaded S3 object', path, s3obj);
+		this.mediaSize = s3obj.ContentLength;
+		this.duration = parseInt(await mediaDuration(s3obj.Body), 10);
+	}
+	
+    async addFile(path) {
+		let oldMedia = this.media;
+		await super.addFile(path);
+		if (oldMedia != this.media) // need to update media metadata
+			await this.loadMediaMetadata(this.media);
+		this.ready = this.media && this.title && this.description &&
+				(Object.keys(this.images).length > 0) &&
+				((new Date()).getTime() > this.canPublish.getTime());
+	}
+
+	toRSS(baseurl) {
+		if (!this.ready)
+			return '';
+		let url = baseurl + '/' + this.keyPrefix;
+		let uuid = uuidv5(url, uuidv5.URL);
+		let largestImageSize = Object.keys(this.images).sort((a,b) => b-a)[0];
+		let largestImage = `${url}${encodeURIComponent(this.images[largestImageSize])}`;
+		let imagesrcset = Object.keys(this.images).map(size => `${url}${encodeURIComponent(this.images[size])} ${size}w`).join(", ");
+		return `
+	<item>
+		<title>${this.title}</title>
+		<description><![CDATA[${this.description}]]></description>
+		<link>${baseurl}/${this.id}</link>
+		<guid isPermaLink="false">${uuid}</guid>
+		<pubDate>${this.publishDate().toRFC822()}</pubDate>
+		<itunes:subtitle>${this.title}</itunes:subtitle>
+		<itunes:summary><![CDATA[${this.description}]]></itunes:summary>
+		<itunes:author>${author}</itunes:author>
+		<author>${author}</author>
+		<itunes:image href="${largestImage}"/>
+		<itunes:explicit>no</itunes:explicit>
+		<itunes:keywords>${this.keywords}</itunes:keywords>
+		<enclosure url="${url}${this.media}" type="audio/mpeg" length="${this.mediaSize}"/>` +
+		/*
+		 *	<podcast:person group="cast" role="host" img="https://feeds.podcastindex.org/adam_avatar.jpg" href="http://curry.com">Adam Curry</podcast:person>
+		 *	<podcast:person href="http://dave.sobr.org" img="https://feeds.podcastindex.org/dave_avatar.jpg" group="cast" role="host">Dave Jones</podcast:person>
+		 *	<podcast:socialInteract protocol="activitypub" uri="https://thread.land/podcast/7GoP6LC/95" accountId="@dave" accountUrl="https://podcastindex.social/users/dave"/>
+		 */
+		`
+		<podcast:images srcset="${imagesrcset}"/>
+		<itunes:duration>${this.duration}</itunes:duration>
+	</item>
+		`.trim();
+	}
+}
+
+class Podcast extends PodItem {
+    episodes = [];
+	
+	publishDate() {
+		let targetDate = this.pubdate;
+		for (let e of this.episodes) {
+			if (targetDate.getTime() < e.publishDate().getTime() && e.ready)
+				targetDate = e.publishDate();
+		}
+		return targetDate;
+	}
+	
+    async loadFromBucket(bucket) {
+        this.bucket = bucket;
+        let files = await listBucket(bucket);
+        for (let file of files) {
+            let prefix, path;
+            [prefix, ...path] = file.split('/');
+            if (path.length) {
+                if (path[0].length) // otherwise is just the episode directory node, which is just an S3 console artifact and shouldn't exist
+                    await this.addEpisodeFile(prefix, path.join('/'));
+            } else
+                await this.addFile(prefix);
+        }
+    }
+    
+    async loadMarkdown(path) {
+		await super.loadMarkdown(path);
+		if (this.keywords.length == 0)
+			return;
+		for (let e of this.episodes)
+			e.addKeywords(this.keywords);
+	}
+    
+    async addEpisodeFile(id, path) {
+        for (let ep of this.episodes) {
+            if (ep.id == id)
+                return await ep.addFile(path);
+        }
+        let ep = new Episode(this.bucket, id);
+		ep.addKeywords(this.keywords);
+        this.episodes[this.episodes.length] = ep;
+        console.log("Creating new episode", ep, this.episodes);
+        return await ep.addFile(path);
+    }
+    
+    toRSS(baseurl) {
+		let url = baseurl + '/';
+		let uuid = uuidv5(url, uuidv5.URL);
+		let largestImageSize = Object.keys(this.images).sort((a,b) => b-a)[0];
+		let largestImage = `${url}${encodeURIComponent(this.images[largestImageSize])}`;
+		let imagesrcset = Object.keys(this.images).map(size => `${url}${encodeURIComponent(this.images[size])} ${size}w`).join(", ");
+		return (`
+<rss
+		xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
+		xmlns:podcast="https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md" version="2.0">
+	<channel>
+	<title>${this.title}</title>
+	<description><![CDATA[${this.description}]]></description>
+	<link>${url}</link>
+	<language>en</language>
+	<generator>${GeneratorName}</generator>
+	<pubDate>${this.publishDate().toRFC822()}</pubDate>
+	<lastBuildDate>${new Date().toRFC822()}</lastBuildDate>
+	<podcast:locked owner="${owner_email}">yes</podcast:locked>
+	<managingEditor>${owner_email} (${owner})</managingEditor>
+	<itunes:owner>
+		<itunes:email>${owner_email}</itunes:email>
+		<itunes:name>${owner}</itunes:name>
+	</itunes:owner>
+	<image>
+		<url>${largestImage}</url>
+		<link>${url}</link>
+		<title>${this.title}</title>
+		<description><![CDATA[${this.description}]]></description>
+		<width>${largestImageSize}</width>
+		<height>${largestImageSize}</height>
+	</image>
+	<itunes:summary><![CDATA[${this.description}]]></itunes:summary>
+	<itunes:author>${author}</itunes:author>
+	<itunes:image href="${largestImage}"/>
+	<itunes:explicit>no</itunes:explicit>
+	<itunes:keywords>${this.keywords}</itunes:keywords>
+	` + categories.map(cat => `<itunes:category text="${cat}"/>`).join("") + `
+	<podcast:guid>${uuid}</podcast:guid>
+	<podcast:medium>podcast</podcast:medium>
+	<podcast:images srcset="${imagesrcset}"/>
+		` + this.episodes.map(e => e.toRSS(baseurl)).join("\n") + `
+	</channel>
+</rss>
+		`).trim();
+	}
+
+    static async load(bucket) {
+        let p = new Podcast();
+        await p.loadFromBucket(bucket);
+        return p;
+    }
+}
+
+exports.handler = async (event, context) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+	let bucket = '';
+	if (event.source == "aws.events") { // scheduled refresh and not an S3 update
+		bucket = process.env.bucket; // try to read bucket from the configuration
+	} else {
+    	// Get the object from the event and show its content type
+    	bucket = event.Records[0].s3.bucket.name;
+    	let key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    	if (key.endsWith('rss')) {
+        	console.log("Ignoring changes to RSS feed itself...");
+        	return "OK";
+    	}
+	}
+	
+	if (!bucket) {
+		console.log("Cannot determine source S3 bucket! Set the `bucket` environment variable to specify the S3 bucket to process");
+		return "OK";
+	}
+    
+    try {
+        let podcast = await Podcast.load(bucket);
+        console.log('Podcast:', podcast);
+		let storeRes = await storeRSS(podcast.toRSS(baseURL), bucket, 'rss');
+		console.log('Updated podcast RSS:', storeRes.ETag);
+        return "OK";
+    } catch (err) {
+        console.log(err);
+        throw new Error(`Failed to generate RSS feed for ${podcast_name}: ${err}`);
+    }
+};
